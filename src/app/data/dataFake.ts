e5e1e35badc91225e889fc6d5252148d1389a641
robertera <robertokorzun@hotmail.com>
2023-08-28 18:45:30 -0300

    Blog Completo (Versão muito basica)

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Baldurs Gate 3 Goty",
+    "description":"Baldurs Gate 3 é um deleite para os fãs de RPG e vem tido uma nota altissima em poucas semanas de lançamento batendo titulos como The Legend of Zelda: Tears of the Kingdom",
+    "photoCover":"https://blog.br.playstation.com/tachyon/sites/4/2023/02/babee9c954509a0d34a67e7d125b679096ef9d79-scaled.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Starfield promete revolucionar em tamanho diz produtor",
+    "description":"Starfield promete ser o que No Men's Sky foi, dando maior vastidão de planetas e coisas para fazer bem como npcs e eventos",
+    "photoCover":"https://sm.ign.com/ign_br/gallery/s/starfield-/starfield-xbox-games-showcase-2023-trailer_pmcm.jpg"
+  },
+]
